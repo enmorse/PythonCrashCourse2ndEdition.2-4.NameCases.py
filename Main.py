1493ca0a285ef05da438a564e38683334874f0e1
Morse <enmorse48@gmail.com>
2021-09-19 21:43:09 -0700

    Initial commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+# Use a variable to represent a person's name, and then
+# print that person's name using lowercase, uppercase,
+# and title case.
+
+first_name = "ernest"
+last_name = "morse"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(full_name.lower())
+print(full_name.upper())
+print(full_name.title())
